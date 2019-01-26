afbf900294eb40cb0030532e0571a01fdc3ab08e
rgwch <weirich@elexis.ch>
2019-01-26 15:21:43 +0100

    div

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,36 @@
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.css";
+import './styles.scss'
+import {Letters} from './letters'
+
 export class App {
   message = 'Hello World!';
+  tiles=[
+    {token: "fuenf", text: "fünf"},
+    {token: "zehn", text: "zehn"},
+    {token: "viertel", text: "1/4"},
+    {token: "zwanzig", text: "zwanzig"},
+    {token: "halb", text: "halb"},
+    {token: "vor", text: "vor"},
+    {token: "nach", text: "nach"},
+    {token: "1", text: "eins"},
+    {token: "2", text: "2"},
+    {token: "3", text: "drei"},
+    {token: "4", text: "4"},
+    {token: "5", text: "fünf"},
+    {token: "6", text: "sechs"},
+    {token: "7", text: "sieben"},
+    {token: "8", text: "8"},
+    {token: "9", text: "neun"},
+    {token: "10", text: "10"},
+    {token: "11", text: "elf"},
+    {token: "12", text: "zwölf"},
+  ]
+ 
+  attached(){
+    const l= new Letters()
+    setInterval(()=>{
+      const tokens=l.getTokensForTime(new Date())
+    },30)
+  }
 }
--- src/letters.ts
+++ src/letters.ts
@@ -5,7 +5,8 @@ export class Letters {
     "1": "eins", "2": "zwei", "3": "drei", "4": "vier", "5": "fünf",
     "6": "sechs", "7": "sieben", "8": "acht", "9": "neun",
     "10": " zehn", "11": " elf", "12": " zwölf", Uhr: " Uhr.",
-    viertel: "viertel", halb: "halb ", "20": "zwanzig ", nach: " nach ", vor: " vor "
+    "fuenf":"fünf","zehn":"zehn",
+    viertel: "viertel", halb: "halb ", "zwanzig": "zwanzig ", nach: " nach ", vor: " vor "
   }
 
   public getTokensForTime(date) {
@@ -16,17 +17,17 @@ export class Letters {
     const h = hr.toString()
     switch (Math.round(m.minute() / 5) * 5) {
       case 0: return [h, "Uhr"]
-      case 5: return ['5', 'nach', h]
-      case 10: return ['10', 'nach', h]
+      case 5: return ['fuenf', 'nach', h]
+      case 10: return ['zehn', 'nach', h]
       case 15: return ['viertel', 'nach', h]
-      case 20: return ["20", 'nach', h]
-      case 25: return ["5",'vor','halb', n]
+      case 20: return ["zwanzig", 'nach', h]
+      case 25: return ["fuenf",'vor','halb', n]
       case 30: return ['halb',n]
-      case 35: return ["5",'nach','halb',n]
-      case 40: return ["20",'vor',n]
+      case 35: return ["fuenf",'nach','halb',n]
+      case 40: return ["zwanzig",'vor',n]
       case 45: return ['viertel','vor',n]
-      case 50: return ["10",'vor',n]
-      case 55: return ["5",'vor',n]
+      case 50: return ["zehn",'vor',n]
+      case 55: return ["fuenf",'vor',n]
       default: return ["Error"]
     }
   }
