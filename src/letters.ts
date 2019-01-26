d0f30c7d0287024d02b6ec61552762615edd2bb4
gerry <weirich@elexis.ch>
2019-01-26 12:31:42 +0100

    initial import

---
+++ src/letters.ts
@@ -0,0 +1,8 @@
+export class Letters{
+  public words=[
+    "eins","zwei","drei","vier","fünf","sechs","sieben","acht","neun",
+    "zehn","elf","zwölf",
+    "viertel","halb","zwanzig","nach","vor"
+  ]
+
+}
