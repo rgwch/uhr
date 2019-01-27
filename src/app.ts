6d97628fab5c56b8331659be50194d1b431bf959
gerry <weirich@elexis.ch>
2019-01-27 09:43:57 +0100

    works

--- src/app.ts
+++ src/app.ts
@@ -1,36 +1,41 @@
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.css";
-import './styles.scss'
-import {Letters} from './letters'
+import "./styles.scss";
+import { Letters } from "./letters";
 
 export class App {
-  message = 'Hello World!';
-  tiles=[
-    {token: "fuenf", text: "fünf"},
-    {token: "zehn", text: "zehn"},
-    {token: "viertel", text: "1/4"},
-    {token: "zwanzig", text: "zwanzig"},
-    {token: "halb", text: "halb"},
-    {token: "vor", text: "vor"},
-    {token: "nach", text: "nach"},
-    {token: "1", text: "eins"},
-    {token: "2", text: "2"},
-    {token: "3", text: "drei"},
-    {token: "4", text: "4"},
-    {token: "5", text: "fünf"},
-    {token: "6", text: "sechs"},
-    {token: "7", text: "sieben"},
-    {token: "8", text: "8"},
-    {token: "9", text: "neun"},
-    {token: "10", text: "10"},
-    {token: "11", text: "elf"},
-    {token: "12", text: "zwölf"},
-  ]
- 
-  attached(){
-    const l= new Letters()
-    setInterval(()=>{
-      const tokens=l.getTokensForTime(new Date())
-    },30)
+  message = "Hello World!";
+  tiles = [
+    { token: "fuenf", text: "fünf", display: "inactive" },
+    { token: "zehn", text: "zehn", display: "inactive" },
+    { token: "viertel", text: "1/4", display: "inactive" },
+    { token: "zwanzig", text: "zwanzig", display: "inactive" },
+    { token: "vor", text: "vor", display: "inactive" },
+    { token: "nach", text: "nach", display: "inactive" },
+    { token: "halb", text: "halb", display: "inactive" },
+    { token: "1", text: "eins", display: "inactive" },
+    { token: "2", text: "2", display: "inactive" },
+    { token: "3", text: "drei", display: "inactive" },
+    { token: "4", text: "4", display: "inactive" },
+    { token: "5", text: "fünf", display: "inactive" },
+    { token: "6", text: "sechs", display: "inactive" },
+    { token: "7", text: "7", display: "inactive" },
+    { token: "8", text: "acht", display: "inactive" },
+    { token: "9", text: "neun", display: "inactive" },
+    { token: "10", text: "10", display: "inactive" },
+    { token: "11", text: "elf", display: "inactive" },
+    { token: "12", text: "zwölf", display: "inactive" }
+  ];
+
+  attached() {
+    const l = new Letters();
+    setInterval(() => {
+      const tokens = l.getTokensForTime(new Date());
+      this.tiles.forEach(tile=>{tile.display="inactive"})
+      tokens.forEach(token=>{
+        const tile=this.tiles.find((t=>t.token==token))
+        tile.display="active"
+      })
+    }, 30);
   }
 }
